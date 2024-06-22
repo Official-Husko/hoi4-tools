2560c8524cca692879eb334b714a570dde139f91
Official-Husko <official.husko@gmail.com>
2024-06-22 03:34:16 +0200

    add TemplateUpdateClass

---
+++ modules/mod_template_update.py
@@ -0,0 +1,26 @@
+# Import Standard Libraries
+
+# Import Third-Party Libraries
+
+# Import Local Libraries
+
+
+class ModTemplateUpdate:
+
+    def __init__(self) -> None:
+        pass
+
+    def _get_all_folders(self) -> list:
+        pass
+
+    def _construct_json(self) -> dict:
+        pass
+
+    def _write_json(self) -> None:
+        pass
+
+    def trigger_update(self) -> None:
+        pass
+
+    def trigger_check(self) -> None:
+        pass
